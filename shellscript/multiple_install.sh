9668e457dca254b34bc3821013ad13d647c819e7
chanakyamiddleware42-debug <chanakya.middleware42@gmail.com>
2026-05-03 13:36:48 +0530

    added

---
+++ shellscript/multiple_install.sh
@@ -0,0 +1,20 @@
+#! /bin/bash
+USERID=$(id -u)
+echo "$USERID"
+if [ $USERID -ne 0 ];  then
+   echo " root access"
+   exit 1
+fi
+validate() {
+if [ $? -ne 0 ]; then 
+   echo "$1 failed"
+else 
+    echo "$1 success"
+fi
+}
+
+dnf install nginx -y
+validate "nginx installation"
+
+dnf install mysql -y
+validate "mysql installation"

718989cc975d5d0894072fb4e0de0bdbfe2f9aa0
chanakyamiddleware42-debug <chanakya.middleware42@gmail.com>
2026-05-03 09:26:46 +0530

    added

--- shellscript/02-variable.sh
+++ shellscript/02-variable.sh
@@ -3,5 +3,5 @@ PERSON1=Chanakya
 PERSON2=Nararayanareddy
 echo $PERSON1:: Hi $PERSON2, have u working as  devepos Engineer
 echo $PERSON2:: Hi $PERSON1, yes i'm working as devops engineer and what about you
-echo $PERSON2:: yes. i'm also devops engineer. bye
+echo $PERSON2:: yes. im also devops engineer, bye
 echo $PERSON2:: ok. bye

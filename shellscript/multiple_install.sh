b886f8f4be31fddd2770718138937db76e942586
chanakyamiddleware42-debug <chanakya.middleware42@gmail.com>
2026-05-03 14:40:24 +0530

    added

--- shellscript/multiple_install.sh
+++ shellscript/multiple_install.sh
@@ -3,18 +3,18 @@ USERID=$(id -u)
 echo "$USERID"
 LOG_DIR="/opt/chani"
 mkdir -p /opt/chani
-LOG_FILE="/opt/chani/$0"
+LOG_FILE="/opt/chani/$0" 
 
 
 if [ $USERID -ne 0 ];  then
-   echo " root access"
+   echo " root access" | tee -a $LOG_FILE
    exit 1
 fi
 VALIDATE() {
 if [ $1 -ne 0 ]; then 
-   echo "$2 failed"
+   echo "$2 failed" | tee -a $LOG_FILE
 else 
-    echo "$2 success"
+    echo "$2 success" | tee -a $LOG_FILE
 fi
 }
 dnf install nginx -y &>> $LOG_FILE

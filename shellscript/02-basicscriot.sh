bb641f54d0648eaa2231addd4679e1101111d7cf
chanakyamiddleware42-debug <chanakya.middleware42@gmail.com>
2026-05-03 09:14:05 +0530

    added

---
+++ shellscript/02-basicscriot.sh
@@ -0,0 +1,5 @@
+!#/bin/bash
+echo chankya:: Hi nararyanareddy, have u working as  devepos Engineer"
+echo nararyanareddy: Hi chanakya, yes i'm working as devops engineer" and what about you
+echo chanakya: yes, i'm also devops engineer. bye
+echo narayanareddy: ok, bye

d21f30b03a32af5904f32e498efc069650456b3c
chanakyamiddleware42-debug <chanakya.middleware42@gmail.com>
2026-05-03 11:58:57 +0530

    added

--- shellscript/09-condition.sh
+++ shellscript/09-condition.sh
@@ -1,7 +1,9 @@
 #! /bin/bash
 NUMBER=$1
 if [ $NUMBER -gt 25 ]; then
- echo "$NUMBER is greaterhan 20"
+ echo "$NUMBER is greaterhan 25"
+elif [ $NUMBER -eq 25 ]; then
+ echo "$NUMBER is equal 25"
 else 
- echo "$NUMBER is lessthan 20"
+ echo "$NUMBER is lessthan 25"
 fi

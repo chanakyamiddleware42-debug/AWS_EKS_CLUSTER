5885b87b1cd186509aace21d9865d898c2e11701
chanakyamiddleware42-debug <chanakya.middleware42@gmail.com>
2026-05-03 09:36:00 +0530

    added

---
+++ shellscript/03-ar.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+PERSON1=$1
+PERSON2=$2
+echo $PERSON1:: Hi $PERSON2, have u working as  devepos Engineer
+echo $PERSON2:: Hi $PERSON1, yes im working as devops engineer and what about you
+echo $PERSON2:: yes. im also devo engineer, bye
+echo $PERSON2:: ok. bye
